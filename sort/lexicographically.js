31e089033d861a6d2c012b0f7dae43954b8bba17
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-03-17 15:52:37 +0800

    sort

---
+++ sort/lexicographically.js
@@ -0,0 +1,37 @@
+/**
+ * javascript use lexicography as default
+ * 
+ * 
+ */
+
+let nums1 = [1, 2, 3, 10, 12, 13];
+nums1.sort();
+console.log(nums1); // [ 1, 10, 12, 13, 2, 3 ]
+
+nums1.sort((a, b) => { return a - b; });
+console.log(nums1); // [ 1, 2, 3, 10, 12, 13 ]
+
+
+
+let nums2 = ["a", "banana", "app", "ap", "apply", "apple"];
+nums2.sort((a, b) => { return a < b });
+console.log(nums2); // [ 'a', 'banana', 'app', 'ap', 'apply', 'apple' ] 并没有变化
+
+nums2.sort((a, b) => { return a - b });
+console.log(nums2); // [ 'a', 'banana', 'app', 'ap', 'apply', 'apple' ] 并没有变化
+
+nums2.sort();
+console.log(nums2); // [ 'a', 'ap', 'app', 'apple', 'apply', 'banana' ]
+
+
+let nums3 = ["a", "banana", "app", "ap", "apply", "apple"];
+nums3.sort((a, b) => { 
+    if(a<b){
+        return -1;
+    }
+    else if(a>b){
+        return 1;
+    }
+    return 0;
+ });
+ console.log(nums3);// [ 'a', 'ap', 'app', 'apple', 'apply', 'banana' ] 实现字典序，所以要实现字典序或字典序逆序，也可以通过这种方式
