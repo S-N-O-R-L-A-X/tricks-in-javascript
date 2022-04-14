bf3f6d9c7e028ff4ce57dcb42a0d3f77a48febe2
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-04-14 17:05:35 +0800

    add new tests

--- jest/fn/jest.fn.test.js
+++ jest/fn/jest.fn.test.js
@@ -15,23 +15,19 @@ describe('测试jest.fn()调用',() => {
         expect(mockFn).toBeCalledTimes(1);
 
         expect(mockFn).not.toBeCalledTimes(2);
-        // 断言mockFn传入的参数为1, 2, 3
-        expect(mockFn).toHaveBeenCalledWith(1, 2, 3);
-    })
-
-    it('测试jest.fn()调用', () => {
-        const mockFn = jest.fn();
-        let result = mockFn(1, 2, 3);
-    
-        expect(result).toBeUndefined();//it will return undefined as default
-        
-        expect(mockFn).toBeCalled();// check whether the mock was called
-        
-        expect(mockFn).toBeCalledTimes(1);
-
-        expect(mockFn).not.toBeCalledTimes(2);
         
         expect(mockFn).toHaveBeenCalledWith(1, 2, 3);//judge parameters
         expect(mockFn).not.toHaveBeenCalledWith(1, 2);//judge parameters
-    })
+    });
+
+    it('jest.fn可以设置参数', () => {
+        const mockFn = jest.fn().mockReturnValue('you can set value');
+        expect(mockFn()).toBe('you can set value');
+    });
+
+    it('jest.fn可以写一个函数', () => {
+        const mockFn = jest.fn((a,b) => {return a+b});
+        expect(mockFn(3,4)).toBe(7);
+    });
+
 })
