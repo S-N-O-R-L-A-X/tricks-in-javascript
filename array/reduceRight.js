f22e0c1864dbdb51f80021127c5065845803d253
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-07-07 20:54:13 +0800

    add reduceRight

---
+++ array/reduceRight.js
@@ -0,0 +1,5 @@
+/**
+ * reduceRight is like reduce but traverse in the opposite direction
+ */
+const arr=[5,7,3,10,9,4,6,1,2,8];
+console.log(arr.reduce((pre,cur) => pre+cur)); // 55 this is a sum function
