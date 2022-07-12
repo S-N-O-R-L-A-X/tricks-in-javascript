b5c4504fc386c50c22fc6a07610a9e24b32f4ae7
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-07-12 23:20:27 +0800

    add content to number-array

--- array/typed array/number-rray.js
+++ array/typed array/number-rray.js
@@ -1,15 +1,19 @@
 /**
- * the int32array has the most of the array functions
+ * javascript has 5 kind of arrays, Int8Array, Int16Array, Int32Array, Float32Array, Float64Array
+ * these arrays have the most of the array functions, but they
+ * since the size is defined, you cannot add or remove elements from the array. so you cannot call functions such as concat(),push(),pop()
  * the array has the property of BYTES_PER_ELEMENT, which return the size of the element
  */
 
 const buffer=new ArrayBuffer(4);
 const ints1=new Int32Array(buffer);
+console.log(ints1); // Int32Array(1) [ 0 ]
 console.log(ints1.length); // 1.  every element in array takes 4 bytes
+console.log(ints1.buffer.byteLength) // 4
+console.log(ints1 instanceof Array); // false
 
-const ints2 = new Int32Array(6);
-console.log(ints2); // Int32Array(6) [ 0, 0, 0, 0, 0, 0 ]
-console.log(ints2.buffer.byteLength); // 24 
+const ints2 = new Int8Array(6);
+console.log(ints2.buffer.byteLength); // 6 
 
 const ints3 = new Int32Array([2, 4, 6, 8]);
 console.log(ints3); // Int32Array(4) [ 2, 4, 6, 8 ]
@@ -39,3 +43,15 @@ console.log(Int16Array.BYTES_PER_ELEMENT); // 2
 console.log(Int32Array.BYTES_PER_ELEMENT); // 4
 console.log(Float32Array.BYTES_PER_ELEMENT); // 4
 console.log(Float64Array.BYTES_PER_ELEMENT); // 8
+
+
+const ints6=new Int16Array(floats1);
+console.log(ints6); // [ 3, 2, 1 ]
+const ints7=new Int32Array(floats1);
+console.log(ints7); // [ 3, 2, 1 ]
+
+const floats2=ints3.map(x => x-0.1);
+console.log(floats2); // [1,3,5,7]
+console.log(floats2 instanceof Array); // false
+console.log(floats2 instanceof Float32Array); // false
+console.log(floats2 instanceof Int32Array); // true
