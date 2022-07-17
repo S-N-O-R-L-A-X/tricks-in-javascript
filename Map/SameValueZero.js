73be18b92259a2bc5e3f8ed6fdc7ba8542e1852f
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-07-17 22:58:08 +0800

    add SameValueZero

---
+++ Map/SameValueZero.js
@@ -0,0 +1,14 @@
+/**
+ * to learn more about SameValueZero, see "@/puzzles/equal.js"
+ */
+const m = new Map();
+const a = 0/"", // NaN
+ b = 0/"", // NaN
+ pz = +0,
+ nz = -0;
+alert(a === b); // false
+alert(pz === nz); // true
+m.set(a, "foo");
+m.set(pz, "bar");
+alert(m.get(b)); // foo
+alert(m.get(nz)); // bar 
