ca8a641d6dee160ce9ca85809d9a685a789d63fb
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-07-17 22:38:16 +0800

    add initialization of map

---
+++ Map/initialization.js
@@ -0,0 +1,19 @@
+/**
+ * you can use nested array or iterator to initialize the Map
+ */
+
+const m1 = new Map([
+    ["key1", "val1"],
+    ["key2", "val2"],
+    ["key3", "val3"]
+]);
+   alert(m1.size); // 3 
+
+const m2 = new Map({
+    [Symbol.iterator]: function*() {
+        yield ["key1", "val1"];
+        yield ["key2", "val2"];
+        yield ["key3", "val3"];
+    }
+});
+alert(m2.size); // 3
