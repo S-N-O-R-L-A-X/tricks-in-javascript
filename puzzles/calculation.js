19726007bbcd5f1bf2edcca77cf2650c4c05b946
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-08-11 23:36:30 +0800

    feat(puzzles-002): add strange tricks about math

---
+++ puzzles/calculation.js
@@ -0,0 +1,14 @@
+console.log(2 + "3"); //23
+console.log(2 - "3"); //-1
+
+console.log(0.1 + 0.2 === 0.3); // false
+console.log(0.1 + 0.2 == 0.3); // false
+
+console.log([] + []); // ""
+console.log([] + {}); // [object Object]
+console.log({} + {}); // [object Object][object Object]
+
+console.log(Math.max()); // -Infinity
+console.log(Math.min()); // Infinity
+
+console.log(99999999999999999) // 100000000000000000
