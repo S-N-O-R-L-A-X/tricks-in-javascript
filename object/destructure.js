820b0afb511fb2fb6bc5e5527b4725562c68a9e7
S-N-O-R-L-A-X <10195101536@stu.ecnu.edu.cn>
2022-10-21 12:31:27 +0800

    feat(object-010): refine destructure

--- object/destructure.js
+++ object/destructure.js
@@ -1,5 +1,7 @@
 /**
  * grammar: {property-name:assignment=default-value}=object
+ * number, string can also be destuctured.
+ * destucture uses ToObject() internally to convert original data-structure to object, but null and undefined cannot be converted to object, so it will throw an error.
  */
 
 const person = {
@@ -7,6 +9,23 @@ const person = {
     age: 18,
 }
 
-const { name: person_name = "aaa" } = person;
-console.log(person_name); // Jack
-console.log(name); // ReferenceError: name is not defined
+try {
+    const { name: person_name = "aaa" } = person;
+    console.log(person_name); // Jack
+    console.log(name);
+} catch (err) {
+    console.error(err) // ReferenceError: name is not defined
+}
+
+const { length } = 'foobar';
+console.log(length); // 6 
+const { constructor: c } = 4;
+console.log(c === Number); // true 
+
+try {
+    const { name: person_name = "aaa" } = null;
+    console.log(person_name);
+} catch (err) {
+    console.log(err); // TypeError: Cannot read property 'name' of null
+}
+
