291487a5b1c6b8f6e0b2e825686986fbff4642c2
S-N-O-R-L-A-X <10195101536@stu.ecnu.edu.cn>
2022-11-10 11:03:47 +0800

    feat(object-012): refine nested-destructure

--- object/destructure/nested-destructure.js
+++ object/destructure/nested-destructure.js
@@ -23,3 +23,23 @@ const { name, age, job: { title } } = person;
 console.log(name); // Matt
 console.log(age); // 27
 console.log(title); // Software engineer
+
+const person3 = {};
+try {
+    ({
+        job: {
+            title: person3.job.title // TypeError: Cannot set property 'title' of undefined
+        }
+    } = person);
+}
+catch (err) {
+    console.log(err);
+}
+
+const person4 = { job: {} };
+({
+    job: {
+        title: person4.job.title // TypeError: Cannot set property 'title' of undefined
+    }
+} = person);
+console.log(person4); // { job: { title: 'Software engineer' } }
