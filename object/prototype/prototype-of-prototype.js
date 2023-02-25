569820fac3fea99b92b4277a58693a122e9b5c66
S-N-O-R-L-A-X <snorlax.xu@outlook.com>
2023-02-25 14:22:56 +0800

    feat(object-021): refine prototype

--- object/prototype/prototype-of-prototype.js
+++ object/prototype/prototype-of-prototype.js
@@ -22,3 +22,20 @@ chrome:
  ... 
 }
 */
+
+const person1 = new Person(), person2 = new Person();
+
+console.log(person1 !== Person); // true 
+console.log(person1 !== Person.prototype); // true 
+console.log(Person.prototype !== Person); // true
+
+/** 
+ * 实例通过__proto__链接到原型对象，
+ * 它实际上指向隐藏特性[[Prototype]] 
+ * 
+ * 构造函数通过 prototype 属性链接到原型对象
+ * 
+ * 实例与构造函数没有直接联系，与原型对象有直接联系
+ */
+console.log(person1.__proto__ === Person.prototype); // true 
+conosle.log(person1.__proto__.constructor === Person); // true
